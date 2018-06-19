24c4e9249d9fc97f62c6195e38b9d15b9713d957
Dmitry Polushkin <mdifps@mail.ru>
2018-06-19 19:24:48 +0300

    first commit

---
+++ frontend/config/bootstrap.php
@@ -0,0 +1,18 @@
+<?php
+
+//use yii\base\Security;
+//
+//$security = new Security();
+//$token1 = "tlkTgoOtV9C";
+//$token2 = "tlkTgoOtV9C";
+//
+//
+//$t1 = $security->maskToken($token1);
+//$t2 = $security->maskToken($token2);
+//echo $t1."<br/>".$t2."<br/>";
+//
+//$token1 = $security->unmaskToken($t1);
+//$token2 = $security->unmaskToken($t2);
+//
+//echo $token1."<br/>".$token2."<br/>";
+//die;
---
+++ frontend/web/1.php
@@ -0,0 +1,5 @@
+<?php
+//session_start();
+////$_SESSION['abc'] = 777;
+//echo $_SESSION['abc'];
+//die;
